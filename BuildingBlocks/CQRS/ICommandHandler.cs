fa2e9034fc7c8e65f9bd6dd45ca8d5cbd6757f77
nur farazi <nur369188@gmail.com>
2024-08-13 08:51:22 +0600

    Added: Mapster

--- BuildingBlocks/CQRS/ICommandHandler.cs
+++ BuildingBlocks/CQRS/ICommandHandler.cs
@@ -2,15 +2,13 @@
 
 namespace BuildingBlocks.CQRS;
 
-public interface ICommandHandler<in TCommand> 
-    : ICommandHandler<TCommand, Unit>
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
     where TCommand : ICommand<Unit>
-{ 
+{
 }
 
-public interface ICommandHandler<in TCommand, TResponse> 
-    : IRequestHandler<TCommand, TResponse> 
-    where TCommand : ICommand<TResponse>
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse> 
     where TResponse : notnull
 {
 }
