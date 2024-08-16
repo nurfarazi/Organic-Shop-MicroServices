8ffcb1b3ed6e67f492d2a0ea9bda553f77e0b4c7
nur farazi <nur369188@gmail.com>
2024-08-16 22:42:29 +0600

    Fixed: docker composed

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,15 @@
 using Carter;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
 
-builder.Services.AddMediatR(configuration =>
+builder.Services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+builder.Services.AddMarten(option =>
 {
-    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
-});
+    option.Connection(builder.Configuration.GetConnectionString("Database") ?? string.Empty);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
