985e70bdf835d547d1b6ee787024145b1550f551
nur farazi <nur369188@gmail.com>
2024-08-17 23:19:33 +0600

    Added: Delete api

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
