aeb8f6ea709275c132ee631829aec7feaf70b8e4
nur farazi <nur369188@gmail.com>
2024-08-17 14:18:27 +0600

    Added: get by id

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException(Guid productId)
+        : base($"Product with id {productId} was not found.")
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler(IDocumentSession session)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null) throw new ProductNotFoundException(query.Id);
+
+        return new GetProductByIdResult(product);
+    }
+}
