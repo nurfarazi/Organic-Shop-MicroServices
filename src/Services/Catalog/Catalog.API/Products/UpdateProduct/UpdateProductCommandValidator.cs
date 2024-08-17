182782f7e24db4df32b3f54a7c9a45e83998257e
nur farazi <nur369188@gmail.com>
2024-08-17 23:21:29 +0600

    Updated: Update product

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+
+        RuleFor(command => command.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+        RuleFor(command => command.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
