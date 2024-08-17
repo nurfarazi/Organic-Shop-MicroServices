9955171ed8205392a779dbc920d0c8d61c976acc
nur farazi <nur369188@gmail.com>
2024-08-17 23:01:15 +0600

    Fixed: Validation issue

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductRequestValidator.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductRequestValidator.cs
@@ -2,14 +2,13 @@ using FluentValidation;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
-    public CreateProductRequestValidator()
+    public CreateProductCommandValidator()
     {
-        RuleFor(x => x.Name).NotEmpty();
-        RuleFor(x => x.Category).NotEmpty();
-        RuleFor(x => x.Description).NotEmpty();
-        RuleFor(x => x.ImageFile).NotEmpty();
-        RuleFor(x => x.Price).GreaterThan(0);
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
     }
 }
