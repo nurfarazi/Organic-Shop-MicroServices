2784f972546b0d41f8222f9ca8ee5a168f5bf6f4
nur farazi <nur369188@gmail.com>
2024-08-17 19:52:30 +0600

    Added: Get by category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler(IDocumentSession session)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query,
+        CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
